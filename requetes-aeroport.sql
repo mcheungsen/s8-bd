f9a104fab447c1fc64b89779144f369f1ae03e99
Maeva Cheung Sen <mcheungsen@emi.u-bordeaux.fr>
2023-02-10 16:52:17 +0100

    :sparkles: suite tp requêtes

--- requetes-aeroport.sql
+++ requetes-aeroport.sql
@@ -11,3 +11,74 @@ JOIN vols ON planning.numvol = vols.numvol
 WHERE datevol = '2023-11-10' AND arrivee = 'NCE';
 
 -- 3. Quel est le nom du pilote le plus expérimenté ?--
+SELECT nom
+FROM pilote
+WHERE anciennete = (SELECT MAX(anciennete) FROM pilote);
+
+--4. Donner le nom et l’âge des pilotes qui effectuent les vols aller-retour entre Paris Orly (ORY) et Toulouse (TLS) ?--
+(SELECT nom, age
+FROM pilote
+INNER JOIN planning ON pilote.matricule = planning.matricule
+INNER JOIN vols ON planning.numvol = vols.numvol
+WHERE vols.depart = 'ORY' AND vols.arrivee = 'TLS')
+INTERSECT
+(SELECT nom, age
+FROM pilote
+INNER JOIN planning ON pilote.matricule = planning.matricule
+INNER JOIN vols ON planning.numvol = vols.numvol
+WHERE vols.depart = 'TLS' AND vols.arrivee = 'ORY');
+
+--5. Quels sont les noms des pilotes qui assurent le vol depuis Londres (LHR) ?--
+SELECT DISTINCT nom
+FROM pilote
+INNER JOIN planning ON pilote.matricule = planning.matricule
+INNER JOIN vols ON planning.numvol = vols.numvol
+WHERE depart = 'LHR';
+
+--6. Quelles sont les villes de destination classées selon l’heure de départ des vols dont le nom du pilote commence par la lettre ’S’ ?--
+SELECT arrivee
+FROM vols
+INNER JOIN planning ON vols.numvol = planning.numvol
+INNER JOIN pilote ON planning.matricule = pilote.matricule
+WHERE pilote.nom ~* '^S'
+ORDER BY heure
+
+--7. Quel est le nom du pilote qui assure le plus de vols ? le moins de vols ?--
+SELECT pilote.nom
+FROM pilote
+INNER JOIN planning ON pilote.matricule = planning.matricule
+INNER JOIN vols ON planning.numvol = vols.numvol
+GROUP BY pilote.nom
+ORDER BY COUNT(vols.numvol) --DESC--
+LIMIT 1;
+
+--8. Quels sont les noms des pilotes qui ne vont jamais à Pragues (PRG) ?--
+SELECT DISTINCT pilote.nom
+FROM pilote
+INNER JOIN planning ON pilote.matricule = planning.matricule
+INNER JOIN vols ON planning.numvol = vols.numvol
+WHERE pilote.matricule NOT IN 
+(SELECT pilote.matricule FROM pilote
+INNER JOIN planning ON pilote.matricule = planning.matricule
+INNER JOIN vols ON planning.numvol = vols.numvol
+WHERE arrivee = 'PRG'
+);
+
+--9. Quels sont les vols qui décollent entre 07:00 et 11:00 ?--
+SELECT *
+FROM vols
+WHERE heure > '07:00' AND heure < '11:00';
+
+--10. Donner le nombre de vols par pilote.--
+SELECT COUNT(*), pilote.nom
+FROM planning
+INNER JOIN pilote ON planning.matricule = pilote.matricule
+GROUP BY pilote.nom;
+
+--11. Donner le nombre de vols par type d’avion.--
+SELECT COUNT(numvol), avion.type
+FROM planning
+INNER JOIN avion ON planning.numserie = avion.numserie
+GROUP BY avion.type;
+
+--12. Donner le nombre de vols par type d’avion classé par ordre croissant du nombre de vols.--
