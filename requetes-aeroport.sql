e5e4037d230e5994f84e3b9cf574b903b7b61087
Maeva Cheung Sen <mcheungsen@emi.u-bordeaux.fr>
2023-03-06 19:54:06 +0100

    update tp

--- requetes-aeroport.sql
+++ requetes-aeroport.sql
@@ -82,3 +82,64 @@ INNER JOIN avion ON planning.numserie = avion.numserie
 GROUP BY avion.type;
 
 --12. Donner le nombre de vols par type d’avion classé par ordre croissant du nombre de vols.--
+SELECT COUNT(*), Avion.type
+FROM vols
+INNER JOIN planning ON vols.numvol = planning.numvol
+INNER JOIN avion ON planning.numserie = avion.numserie
+GROUP BY avion.type;
+
+--13. Donner la liste des vols qui partent l'après-midi uniquement--
+SELECT *
+FROM vols
+WHERE heure >= '12:00';
+
+--14. Où est-ce possible d'aller au départ de Londres LHR en passant par Paris CDG--
+--On considère que la planning rend les voyages possibles ?--
+SELECT *
+FROM vols
+INNER JOIN vols AS vols2 ON vols.arrivee = vols2.depart 
+WHERE vols.depart = 'LHR' AND vols.arrivee = 'CDG';
+
+--15. Quel(s) est(sont) le(s) pilote(s) qui réalise(nt) le plus de vols à bord d’un Airbus ?--
+SELECT pilote.nom, COUNT(planning.numvol) as nb
+FROM pilote
+NATURAL JOIN planning
+NATURAL JOIN avion
+WHERE avion.type = 'Airbus'
+GROUP BY pilote.nom
+ORDER BY nb DESC
+
+--16. Quels sont les numéros de série et la localisation des avions qui ne sont pas en service ?--
+SELECT numserie, localisation
+from avion
+WHERE numserie not in (SELECT numserie FROM avion NATURAL JOIN planning);
+
+--17. Quel est l’avion qui dessert tous les vols vers Pragues (PRG) ?--
+SELECT *
+FROM avion
+NATURAL JOIN planning
+NATURAL JOIN vols
+WHERE vols.arrivee = 'PRG' AND avion.numserie not in (
+   SELECT numserie
+   FROM avion 
+   NATURAL JOIN planning 
+   NATURAL JOIN vols 
+   WHERE vols.arrivee != 'PRG');
+
+--18.Quelle est la ligne qui a la plus importante capacité de transport ?--
+SELECT depart, arrivee, SUM(avion.capacite)as nb
+FROM vols
+NATURAL JOIN planning
+NATURAL JOIN avion
+GROUP BY depart,arrivee
+ORDER BY nb DESC
+LIMIT 1;
+
+--19. Quelles lignes ne sont plus desservis ?--
+SELECT depart
+FROM vols
+WHERE depart not in (
+    SELECT arrivee
+    FROM vols);
+
+--20. Est-ce qu’il y a un pilote qui vole sur tous les modèles d’avion ?--
